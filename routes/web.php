5de3b01235ff2fbcd461e251ffbc9b19c7ec610d
lucassm19 <124300806+lucassm19@users.noreply.github.com>
2023-05-02 17:34:48 -0300

    Controle de estoque

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,22 @@
 <?php
-
 use App\Http\Controllers\EstoqueController;
 use Illuminate\Support\Facades\Route;
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/estoque', [EstoqueController::class,'index'])->name('estoque');
+Route::get('/estoque', [EstoqueController::class, 'index'])->name('estoque');
+
+Route::get('/estoque/adicionar', [EstoqueController::class, 'adicionar'])->name('estoque.adicionar');
+
+Route::post('/estoque/adicionar', [EstoqueController::class, 'adicionar']);
 
 // Route::get('/teste', function() {
-//     return 'o teste funcionou';
+//     return 'O teste funcionou';
 // });
-
 // Route::get('/teste-com-view', function() {
 //     return view('teste');
 // });
-
-// Route::get('/noticia/{id}', function($id = 'nada') {
-//     return "Você esta lendo a notícia {$id}";
+// Route::get('/noticia/{id?}', function($id = 'NADA') {
+//     return "Você está lendo a notícia {$id}";
 // });
